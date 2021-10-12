1dfe8bf149aa69048a4ad375b14df77a2ff1ffa7
Henry Schmid <henry.schmid1@gmail.com>
2021-10-12 12:17:42 -0600

    Add association tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Friendship do
   it { should belong_to(:user) }
+  it { should belong_to(:friend).class_name('User') }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe User do
   it { should have_many(:friendships) }
   it { should have_many(:guests) }
-  it { should have_many(:parties).through (:guests) }
+  it { should have_many(:parties).through(:guests) }
 end
