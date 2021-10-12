fda6e7c38d3b2cf7f362904ea5d3dd37edb7f3eb
Jessica Jones <daddehjones@gmail.com>
2021-10-12 17:27:32 -0600

    Add validations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_many :guests, dependent: :destroy
   has_many :parties, through: :guests
   has_many :friends, through: :friendships
+
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+
+  has_secure_password
 end
