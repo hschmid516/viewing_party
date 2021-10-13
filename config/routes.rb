05c740291df2ec74fcff8cbb7b83661f1823843a
Henry Schmid <henry.schmid1@gmail.com>
2021-10-12 20:23:50 -0600

    Add registration and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  resources :dashboard
   post '/login', to: 'users#login'
+  get '/registration', to: 'users#new'
 
+  resources :dashboard
+  resources :users, only: [:new, :create]
 end
