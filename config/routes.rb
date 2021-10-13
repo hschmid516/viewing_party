016b0f2c4fa34f9c3bc7326bf1fd361d8fc28f4f
Jessica Jones <daddehjones@gmail.com>
2021-10-13 18:15:53 -0400

    Handroll login and logout routes.
    Co-Authored-By: Henry Schmid <henry.schmid1@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  post '/login', to: 'users#login'
   get '/registration', to: 'users#new'
 
   resources :dashboard
   resources :users, only: [:new, :create]
   resources :discover, only: :index
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
