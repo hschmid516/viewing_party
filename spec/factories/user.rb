70cbddb77a11d2a04e5670cd0ffdf49f201d10be
Jessica Jones <daddehjones@gmail.com>
2021-10-13 18:13:41 -0400

    Modify faker password.
    Co-Authored-By: Henry Schmid <henry.schmid1@gmail.com>

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user, class: User do
     email { Faker::Internet.unique.email }
-    password_digest { Faker::Alphanumeric.alphanumeric(number: 30) }
+    password { Faker::Alphanumeric.alphanumeric(number: 30) }
     name { Faker::Name.unique.first_name }
     created_at { "2012-03-27 14:53:59 UTC" }
     updated_at { "2012-03-27 14:53:59 UTC" }
