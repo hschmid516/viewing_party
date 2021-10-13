1ccf28122f3b864e9937f81a01f55536de317946
Jessica Jones <daddehjones@gmail.com>
2021-10-13 18:13:24 -0400

    Create dashboard controller with require_user action.
    Co-Authored-By: Henry Schmid <henry.schmid1@gmail.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,10 @@
 class DashboardController < ApplicationController
+  before_action :require_user
 
+  def require_user
+    unless current_user
+      flash[:warning] = "You must be logged in to access this page! Access denied!"
+      redirect_to root_path
+    end
+  end
 end
