fba767502ae8a17921c93dfe9f886eb5c6e3351e
Henry Schmid <henry.schmid1@gmail.com>
2021-10-13 21:24:40 -0600

    Add sad path handling for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :require_user
+
   def new
     @user = User.new
   end
@@ -7,11 +9,14 @@ class UsersController < ApplicationController
     user = user_params
     user[:email] = user[:email].downcase
     new_user = User.create(user)
-    if new_user
+    if new_user.save
       session[:user_id] = new_user.id
       flash[:success] = "Welcome, #{new_user.name}!"
+      redirect_to dashboard_index_path
+    else
+      flash[:error] = 'Email is taken, or password is invalid. Try again'
+      redirect_to registration_path
     end
-    redirect_to dashboard_index_path
   end
 
   private
