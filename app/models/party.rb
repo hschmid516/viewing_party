78e0ed65c715ffa9450b1c519e4198fd4276fe10
Henry Schmid <henry.schmid1@gmail.com>
2021-10-14 17:18:41 -0600

    Add #find_host, #invited
    Co-Authored-By: Jessica Jones <daddehjones@gmail.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,12 @@
 class Party < ApplicationRecord
   has_many :guests, dependent: :destroy
   has_many :users, through: :guests
+
+  def find_host
+    guests.where(host: true).first.user.name
+  end
+
+  def invited
+    guests.where(host: false)
+  end
 end
