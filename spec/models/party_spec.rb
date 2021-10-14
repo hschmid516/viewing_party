e46096c5ec7db7d50c057103e2c080f53eee7300
Henry Schmid <henry.schmid1@gmail.com>
2021-10-14 17:18:27 -0600

    Add tests for #find_host, #invited
    Co-Authored-By: Jessica Jones <daddehjones@gmail.com>

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -3,4 +3,25 @@ require 'rails_helper'
 RSpec.describe Party do
   it { should have_many(:guests) }
   it { should have_many(:users).through(:guests) }
+
+  before :each do
+    @user1 = create(:user)
+    @user2 = create(:user)
+    @user3 = create(:user)
+    @party1 = create(:party)
+    @party2 = create(:party)
+    @user1.friendships.create(friend: @user2)
+    @user1.friendships.create(friend: @user3)
+    @guest1 = @party1.guests.create(user: @user1, host: true)
+    @guest2 = @party1.guests.create(user: @user2, host: false)
+    @guest3 = @party1.guests.create(user: @user3, host: false)
+  end
+
+  it 'can find host' do
+    expect(@party1.find_host).to eq(@user1.name)
+  end
+
+  it 'can find invited guests' do
+    expect(@party1.invited).to eq([@guest2, @guest3])
+  end
 end
