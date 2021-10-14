0ec3f2f6bdc364c84bec7ff22b6f63d611d31ae3
Henry Schmid <henry.schmid1@gmail.com>
2021-10-13 21:24:06 -0600

    Move #require_user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  
+  before_action :require_user
+
   def current_user
     User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    unless current_user
+      flash[:warning] = "You must be logged in to access this page! Access denied!"
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,4 @@
 class DashboardController < ApplicationController
-  before_action :require_user
-
-  def require_user
-    unless current_user
-      flash[:warning] = "You must be logged in to access this page! Access denied!"
-      redirect_to root_path
-    end
-  end
-
   def index
     @user = current_user
   end
