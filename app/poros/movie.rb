43640924023bb54ad25717ff2dd332db3c49e41e
Henry Schmid <henry.schmid1@gmail.com>
2021-10-16 09:23:18 -0600

    Add movie service and PORO
    Co-Authored-By: Jessica Jones <daddehjones@gmail.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -2,23 +2,23 @@ class Movie
 
   attr_reader :id,
               :title,
-              :vote_average,
-              :run_time,
-              :genres,
-              :summary,
-              :cast,
-              :total_reviews,
-              :reviews
+              :vote_average
+              # :run_time,
+              # :genres,
+              # :summary,
+              # :cast,
+              # :total_reviews,
+              # :reviews
 
   def initialize(info)
     @id = info[:id]
     @title = info[:title]
-    @vote_average = info[:]
-    @run_time = info[:]
-    @genres = info[:]
-    @summary = info[:]
-    @cast = info[:]
-    @total_reviews = info[:]
-    @reviews = info[:]
+    @vote_average = info[:vote_average]
+    # @run_time = info[:]
+    # @genres = info[:]
+    # @summary = info[:]
+    # @cast = info[:]
+    # @total_reviews = info[:]
+    # @reviews = info[:]
   end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -8,9 +8,12 @@ class MovieService
       JSON.parse(data, symbolize_names:  true)
     end
 
-    def top_rated
-      page_1 = get_data("#{BASE_URL}/movie/top_rated?api_key=#{ENV['API_KEY']}&language=en-US&page=1")
-      page_2 = get_data("#{BASE_URL}/movie/top_rated?api_key=#{ENV['API_KEY']}&language=en-US&page=2")
+    def top_40
+      top_rated(1) + top_rated(2)
+    end
+
+    def top_rated(page)
+      get_data("#{BASE_URL}/movie/top_rated?api_key=#{ENV['API_KEY']}&language=en-US&page=#{page}")[:results]
     end
 
     def movie_reviews(movie_id)
