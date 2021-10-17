68c8911f1d4f5167bea08172576ec2daae1beecc
Jessica Jones <daddehjones@gmail.com>
2021-10-17 17:58:10 -0400

    Refactor details.
    Co-Authored-By: Henry Schmid <henry.schmid1@gmail.com>

--- app/poros/movie_details.rb
+++ app/poros/movie_details.rb
@@ -1,20 +1,26 @@
 class MovieDetails
 
   attr_reader :id,
+              :title,
+              :vote_average,
               :run_time,
               :genres,
               :summary,
               :cast,
               :total_reviews,
+              :author,
               :reviews
 
-  def initialize(info)
-    @id = info[:id]
-    @run_time = info[:run_time]
-    @genre = info[:genre]
-    @summary = info[:summary]
-    @cast = info[:cast]
-    @total_reviews = info[:total_reviews]
-    @review = info[:review]
+  def initialize(details, cast, reviews)
+    @id = details[:id]
+    @title = details[:title]
+    @vote_average = details[:vote_average]
+    @run_time = details[:run_time]
+    @genres = details[:genres].map {|genre| genre[:name]}
+    @summary = details[:overview]
+    @cast = cast[:cast]
+    @total_reviews = reviews[:total_reviews]
+    @reviews = reviews[:review]
+    @author = reviews[:author]
   end
 end
