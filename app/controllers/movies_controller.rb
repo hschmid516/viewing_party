511f22ddf30d0b0f61f2eaf27c988f191ac0277b
Henry Schmid <henry.schmid1@gmail.com>
2021-10-18 16:16:25 -0600

    Create movie facade and move logic from controller
    Co-Authored-By: Jessica Jones <daddehjones@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,13 @@
 class MoviesController < ApplicationController
   def index
     if params[:top_rated]
-      movie_data = MovieService.top_40
-      @movies = movie_data.map do |movie_info|
-        Movie.new(movie_info)
-      end
+      @movies = MovieFacade.top_movies
     elsif params[:search]
-      movie_data = MovieService.top_40_searched(params[:search])
-      @movies = movie_data.map do |movie_info|
-        Movie.new(movie_info)
-      end
+      @movies = MovieFacade.find_movies(params[:search])
     end
   end
 
   def show
-    details = MovieService.details(params[:id])
-    cast = MovieService.cast(params[:id])
-    reviews = MovieService.reviews(params[:id])
-    @movie = MovieDetails.new(details, cast, reviews)
+    @movie = MovieFacade.movie_details(params[:id])
   end
 end
