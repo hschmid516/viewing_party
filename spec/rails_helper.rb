5b625f9685a5892d8090be1f2f89af205a52e87b
Henry Schmid <henry.schmid1@gmail.com>
2021-10-18 15:38:23 -0600

    Add configure_rspec_metadata and debug_logger

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -80,6 +80,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.configure_rspec_metadata!
   config.filter_sensitive_data('<API_KEY>') { ENV['API_KEY'] }
+  # config.debug_logger = $stderr
   # config.default_cassette_options = { re_record_interval: 5.minute }
 end
