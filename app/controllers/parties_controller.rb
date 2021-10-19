29cf147db6512a1e36a949c1c598cd96b5077267
Jessica Jones <daddehjones@gmail.com>
2021-10-18 21:59:34 -0400

    Create create action.
    Co-Authored-By: Henry Schmid <henry.schmid1@gmail.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,20 @@
 class PartiesController < ApplicationController
   def new
-    
+      @movie = MovieFacade.movie_details(params[:movie_id])
+  end
+
+  def create
+    #create party and guest
+    party = Party.create(party_params)
+    party.guests.create(user: current_user, host: true)
+    params[:friend_ids].each do |friend_id|
+      party.guests.create(user_id: friend_id, host: false)
+    end
+    redirect_to dashboard_index_path
+  end
+
+  private
+  def party_params
+   params.permit(:duration, :day, :time, :title)
   end
 end
