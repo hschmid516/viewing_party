66ce295ae9c258edf2b0b729ded2014f977a997d
Henry Schmid <henry.schmid1@gmail.com>
2021-10-20 14:57:54 -0600

    Add movie_id to parties
    Co-Authored-By: Jessica Jones <daddehjones@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_040312) do
+ActiveRecord::Schema.define(version: 2021_10_20_201544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_040312) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
