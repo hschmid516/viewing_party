82cfef51a7964b1e18e3166bc74d8a111f25ccfb
Henry Schmid <henry.schmid1@gmail.com>
2021-10-19 19:37:42 -0600

    Refactor tests
    Co-Authored-By: Jessica Jones <daddehjones@gmail.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -4,47 +4,38 @@ RSpec.describe 'Movies#show', :vcr do
   before :each do
     @user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-  end
-
-  it 'has movie details' do
     details = MovieService.details(17473)
     cast = MovieService.cast(17473)
     reviews = MovieService.reviews(17473)
-    movie = MovieDetail.new(details, cast, reviews)
-    visit movie_path(movie.id)
+    @movie = MovieDetail.new(details, cast, reviews)
+    visit movie_path(@movie.id)
+  end
 
-    expect(page).to have_content(movie.title)
-    expect(page).to have_content(movie.vote_average)
-    expect(page).to have_content(movie.runtime)
-    expect(page).to have_content(movie.summary)
-    expect(page).to have_content(movie.total_reviews)
+  it 'has movie details' do
+    expect(page).to have_content(@movie.title)
+    expect(page).to have_content(@movie.vote_average)
+    expect(page).to have_content(@movie.runtime)
+    expect(page).to have_content(@movie.summary)
+    expect(page).to have_content(@movie.total_reviews)
 
-    movie.genres.each do |genre|
+    @movie.genres.each do |genre|
       expect(page).to have_content(genre)
     end
 
-    movie.cast.each do |actor, character|
+    @movie.cast.each do |actor, character|
       expect(page).to have_content(actor)
       expect(page).to have_content(character)
     end
 
-    expect(movie.cast.length).to eq(10)
+    expect(@movie.cast.length).to eq(10)
 
-    movie.reviews.each do |author, review|
+    @movie.reviews.each do |author, review|
       expect(page).to have_content(author)
       expect(page).to have_content(review.tr("\n", ' '))
     end
   end
 
   it 'has button for creating party' do
-    details = MovieService.details(17473)
-    cast = MovieService.cast(17473)
-    reviews = MovieService.reviews(17473)
-
-    movie = MovieDetail.new(details, cast, reviews)
-
-    visit movie_path(movie.id)
-
     click_button "Create Viewing Party for Movie"
 
     expect(current_path).to eq(new_party_path)
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -5,26 +5,25 @@ RSpec.describe 'Parties#new', :vcr do
     @user1 = create(:user)
     @user2 = create(:user)
     @user3 = create(:user)
+    @user4 = create(:user)
     @user1.friendships.create(friend: @user2)
     @user1.friendships.create(friend: @user3)
-
+    @user1.friendships.create(friend: @user4)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
-
     visit movie_path(238)
     click_button 'Create Viewing Party for Movie'
   end
 
   it "can create a new viewing party" do
     expect(current_path).to eq(new_party_path)
-    save_and_open_page
+    expect(page).to have_field('duration', with: '175')
+
     fill_in :title, with: 'The Godfather'
     fill_in :duration, with: 500
     fill_in :day, with: '12/12/2021'
     fill_in :time, with: '07:15'
-
     check @user2.name
     check @user3.name
-
     click_button 'Create Party'
 
     expect(current_path).to eq(dashboard_index_path)
@@ -37,6 +36,12 @@ RSpec.describe 'Parties#new', :vcr do
     within("#invited") do
       expect(page).to have_content(@user2.name)
       expect(page).to have_content(@user3.name)
+      expect(page).to_not have_content(@user4.name)
     end
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user2)
+    visit dashboard_index_path
+
+    expect(page).to have_content('The Godfather')
   end
 end
