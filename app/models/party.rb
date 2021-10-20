25ebd18443e78a07c463f3d40701f93beb239f21
Henry Schmid <henry.schmid1@gmail.com>
2021-10-19 19:42:21 -0600

    Add min runtime for parties
    Co-Authored-By: Jessica Jones <daddehjones@gmail.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,8 @@
 class Party < ApplicationRecord
   has_many :guests, dependent: :destroy
   has_many :users, through: :guests
+  validates :duration, numericality: { only_integer: true }
+  validates :duration, :day, :time, presence: true
 
   def find_host
     guests.where(host: true).first.user.name
